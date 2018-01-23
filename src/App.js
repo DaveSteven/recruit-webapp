1973b14ab63215e4999c35cbcab995d05a39058f
David <hanwei@yunwii.com>
2018-01-23 22:49:31 +0800

    添加redux

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import React from 'react';
+import { Button } from 'antd-mobile';
+import { connect } from 'react-redux';
+import { addGun, removeGun, addGunAsync } from './index.redux.js';
 
-class App extends React.Component{
-  componentWillMount() {
-    console.log('组件马上加载')
-  }
-  componentDidMount() {
-    console.log('组件加载完毕')
-  }
+class App extends React.Component {
   render() {
-    console.log('组件正在加载')
+    const num = this.props.num;
+    const addGun = this.props.addGun;
+    const removeGun = this.props.removeGun;
+    const addGunAsync = this.props.addGunAsync;
+
     return (
-      <h2>独立团</h2>
-    )
+      <div>
+        <p>现在有机枪{num}把</p>
+        <Button onClick={addGun}>申请机关枪</Button>
+        <Button onClick={removeGun}>上交机关枪</Button>
+        <Button onClick={addGunAsync}>晚两天再给机关枪</Button>
+      </div>
+    );
   }
 }
 
+const mapsStateToProps = state => {
+  return { num: state };
+}
+const actionCreators = { addGun, removeGun, addGunAsync };
+App = connect(mapsStateToProps, actionCreators)(App);
 export default App;
