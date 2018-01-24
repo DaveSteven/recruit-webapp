05ddda8897583b94ab640bd3f4ab034586bfe203
David <hanwei@yunwii.com>
2018-01-24 16:39:40 +0800

    添加redux

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { connect } from 'react-redux';
+import { addGun, removeGun, addGunAsync } from './index.redux';
+import { Button } from 'antd-mobile';
 
+@connect(state => ({ num: state }), { addGun, removeGun, addGunAsync })
 class App extends Component {
   render() {
     return (
@@ -10,9 +14,10 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="App-intro">目前有{this.props.num}把机关枪</p>
+        <Button onClick={this.props.addGun}>申请机关枪</Button>
+        <Button onClick={this.props.removeGun}>上交机关枪</Button>
+        <Button onClick={this.props.addGunAsync}>延迟上交</Button>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { counter } from './index.redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+  counter,
+  compose(
+    applyMiddleware(thunk),
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  )
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
