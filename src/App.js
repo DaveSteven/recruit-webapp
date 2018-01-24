d83b0c989af71e21c156058db0e77445a2090b23
David <hanwei@yunwii.com>
2018-01-24 10:51:02 +0800

    update

--- src/App.js
+++ src/App.js
@@ -5,17 +5,12 @@ import { addGun, removeGun, addGunAsync } from './index.redux.js';
 
 class App extends React.Component {
   render() {
-    const num = this.props.num;
-    const addGun = this.props.addGun;
-    const removeGun = this.props.removeGun;
-    const addGunAsync = this.props.addGunAsync;
-
     return (
       <div>
-        <p>现在有机枪{num}把</p>
-        <Button onClick={addGun}>申请机关枪</Button>
-        <Button onClick={removeGun}>上交机关枪</Button>
-        <Button onClick={addGunAsync}>晚两天再给机关枪</Button>
+        <p>现在有机枪{this.props.num}把</p>
+        <Button onClick={this.props.addGun}>申请机关枪</Button>
+        <Button onClick={this.props.removeGun}>上交机关枪</Button>
+        <Button onClick={this.props.addGunAsync}>晚两天再给机关枪</Button>
       </div>
     );
   }
