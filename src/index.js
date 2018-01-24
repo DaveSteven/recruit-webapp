9f24cedadf1684133999c085bdb6b6c7b99eecdb
David <hanwei@yunwii.com>
2018-01-24 18:39:45 +0800

    add router

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { counter } from './index.redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Link, Redirect, Switch } from 'react-router-dom';
 
 const store = createStore(
   counter,
@@ -16,9 +17,48 @@ const store = createStore(
   )
 );
 
+class Sumey extends React.Component {
+  render() {
+    console.log(this.props);
+    return <h1>Sumey是小仙女。</h1>;
+  }
+}
+
+function David() {
+  return <h1>David最爱小仙女。</h1>;
+}
+
+class Test extends React.Component {
+  render() {
+    console.log(this.props);
+    return <h2>{this.props.match.params.location}</h2>;
+  }
+}
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">根目录</Link>
+          </li>
+          <li>
+            <Link to="/Sumey">小仙女是谁？</Link>
+          </li>
+          <li>
+            <Link to="/David">谁最爱小仙女？</Link>
+          </li>
+        </ul>
+        {/* <Redirect to="/sumey" component={Sumey}></Redirect> */}
+        <Switch>
+          <Route path="/" exact component={App} />
+          <Route path="/sumey" component={Sumey} />
+          <Route path="/david" component={David} />
+          <Route path="/:location" component={Test} />
+        </Switch>
+      </div>
+    </Router>
   </Provider>,
   document.getElementById('root')
 );
